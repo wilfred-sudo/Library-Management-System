f0ef5ba023fa7ceff538808d2b5f313fb3eccdd2
yusufmim <yusufmim2001@gmail.com>
2025-06-30 16:03:18 +0300

    Fix SQLAlchemy initialization in run.py

--- server/run.py
+++ server/run.py
@@ -1,13 +1,14 @@
 from app import create_app
-from app.models import db
-import seed
-import os
+import os 
 
 app = create_app()
 
-with app.app_context():
-    db.create_all()  # Create tables if they don't exist
-    seed.seed_data()  # Seed the database
-
-if __name__ == '__main__':
+if __name__ == '__main__': 
+    with app.app_context():
+        from app.models import db, User, Book, BorrowRecord, Review  # Import models inside context
+        db.create_all()  # Create tables if they don't exist
+        # Check if tables are empty and seed if necessary
+        if User.query.count() == 0 and Book.query.count() == 0:
+            import seed
+            seed.seed_data()
     app.run(debug=True, host='0.0.0.0', port=int(os.getenv('PORT', 5000)))
